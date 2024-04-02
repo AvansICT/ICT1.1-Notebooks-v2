bb2217bb7e5ab6cafafd18be674d42dcfc8824d5
Borf <git@borf.nl>
2024-04-02 10:53:12 +0200

    Nog niet gefixt

--- _static/custom.js
+++ _static/custom.js
@@ -1,6 +1,10 @@
 document.onload = function() {
+    console.log("Initializing");
     elements = document.querySelectorAll(".highlight-mermaid pre span");
+    console.log(elements.length + " spans");
     elements.forEach(e => e.remove());
+    elements = document.querySelectorAll(".highlight-mermaid pre span");
+    console.log(elements.length + " spans after remove");
 
     mermaid.initialize({ startOnLoad: false });    
     mermaid.run({
